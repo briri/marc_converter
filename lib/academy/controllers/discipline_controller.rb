b5993d11c5e83753decba2a0514e2fc940c8c935
briley <brian.riley@ucop.edu>
2015-05-05 09:31:38 -0700

    Added some debug to the controller in an attempt to consolidate regex patterns further

--- lib/academy/controllers/discipline_controller.rb
+++ lib/academy/controllers/discipline_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../study_hall.rb'
+require_relative '../../helper.rb'
 
 module Cdl
   class MarcAcademy < Sinatra::Application
@@ -35,31 +36,128 @@ sliced = patterns[0].split(/{\d}/)
 
 # Use pattern matching to retrieve the {\d} values and consolidate them where it makes sense!
 
+puts "Pattern: #{patterns[0]}"
+
 puts "Sliced: #{patterns[0].split(/{\d}/)}"
 
-puts "Character Arrays: #{chr_arrays}"
-      
-# [["^", "[", "\\", "(", "]", "[", "a", "-", "z", "A", "-", "Z", "]", "{", "5", "}", "[", "\\", ")", "]", "[", "0", "-", "9", "]", "{", "8", "}"], 
-# ["^", "[", "\\", "(", "]", "[", "a", "-", "z", "A", "-", "Z", "]", "{", "5", "}", "[", "\\", ")", "]", "[", "a", "-", "z", "A", "-", "Z", "]", "{", "3", "}", "[", "0", "-", "9", "]", "{", "8", "}", "\\", "s"], 
-# ["^", "[", "a", "-", "z", "A", "-", "Z", "]", "{", "3", "}", "[", "0", "-", "9", "]", "{", "8", "}"]]
-      
-new_strs = chr_arrays.collect{|arr| "^#{arr[1]}"}
+puts "Matched 1: #{patterns[0].match(/\[[a-zA-Z0-9]+\]/)}"
+puts "Matched 2: #{patterns[0].match(/\{\d+\}/)}"
 
-chr_arrays = chr_arrays.sort{|x,y| y.size <=> x.size}
+cntr, parts, sizes = 0, {}, {}
 
-max = chr_arrays[1].size
+patterns.each do |pattern|
+  pat = pattern_to_hash(pattern.to_s)
   
-chr_arrays.each do |array|
-  i = 0
-
-  begin
-    new_strs.find{|val| val[0..i].eql?(array)}
-    
-    i += 1
-  end while i < max
+  unless pat.nil?
+    parts[cntr] = pat
+    sizes[cntr] = pat.size
+    cntr += 1
+  end
 end
 
-puts "New Strings: #{new_strs}"
+sz = sizes.values.uniq.sort.reverse#_by{|k,v| v}
+sorted = []
+
+sz.each do |s|
+  sizes.find_all{|k,v| v.eql?(s)}.each do |key, value|
+    sorted << parts[key]
+  end
+end
+
+i, out, current = 0, {}, ""
+
+while i < (sorted[0].size - 1)
+  prior = current
+  
+  
+  sorted.each do |parts|
+puts parts[i]
+puts "**************"
+    
+    current = "#{prior}#{parts[i].to_s}"
+    
+    unless parts[i].nil?
+
+puts "#{prior} <--> #{current}"
+puts parts[i]
+      
+      if !parts[i].match(/\{\d\}/).nil?
+        masked = "#{current.sub(/\{\d\}/, '{?}')}"
+        nbr = parts[i][(parts[i].index('{') + 1)]
+      
+        if out[prior].nil?
+          out[masked] = [nbr]
+          
+        else
+          out[masked] = (out[prior] << nbr).uniq
+          out.delete(prior)
+        end
+      
+      else
+        if out[current].nil?
+          out[current] = []
+        
+          out.delete(prior)
+        end
+      end
+    end
+  end
+  
+  i += 1
+end
+
+puts "--------------------------------------------------------------"
+puts "#{out}"
+
+
+
+=begin
+patterns.each do |pattern|
+  a, pos, old, cntr = "", 0, 0, 1
+  
+  while !pos.nil? and !a.nil?
+    old = pos + a.to_s.size unless a.nil?
+  
+    a = pattern.match(/\{\d+\}/, old + 1)
+    pos = pattern.index(a.to_s, old + 1)
+  
+    unless pos.nil?
+      before = pattern[old..(pos - 1)]
+      parts[cntr] = {} if parts[cntr].nil?
+      parts[cntr][before] = [] if parts[cntr][before].nil?
+      
+      parts[cntr][before] << a.to_s.sub('{','').sub('}','').to_i unless parts[cntr][before].include?(a)
+      cntr += 1
+    end
+  end
+end
+
+puts parts
+
+parts.each do |pos, pattern|
+  pattern.each do |p, lengths|
+    sects, curr, i, j = [], "", 0, 0
+  
+    i = 1 if p.start_with?('^')
+    
+    while !curr.nil?
+      curr = p.match(/\[.+\]/, i + 1)
+      j = p.index(curr.to_s, i + 1)
+    
+      unless j.nil?
+        s = p[i..(j - 1)]
+        sects << s
+      end
+    end
+  
+    puts "section #{pos} - #{p} >> #{sects}"
+  end
+end
+=end
+# ^ [ \ ( ] [ a - z A  -  Z  ]  {  5  }  [  \  )  ]  [  0  -  9  ]  {  8  }
+# 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28
+
+# 0 - 13  |  14 - 16  |  17 - 25  | 26 - 28
 
       erb :discipline
     end
--- lib/helper.rb
+++ lib/helper.rb
@@ -14,3 +14,74 @@ def to_camel_case(val)
   parts = val.split('_')
   parts.collect{ |part| "#{part.capitalize}" }.join('')
 end
+
+# -------------------------------------------------------------
+def pattern_to_hash(pattern)
+#  parts = pattern_to_array(pattern, /\{\d+\}/, '{}')
+
+  parts = p_to_a(pattern, /\[(a-zA-Z|0-9|,|\\\\\.)\](\{\d+\})?/)  
+  
+puts "parts: #{parts}"
+
+  parts  
+end
+
+# -------------------------------------------------------------
+def p_to_a(pattern, match_on)
+  parts, pos, last, part = ["#{pattern.slice(0,1)}"], 0, 0, ""
+  
+  while !pos.nil? and !part.nil?
+    last = pos + part.to_s.size unless part.nil?
+    
+    part = pattern.match(match_on, last + 1)
+    pos = pattern.index(part.to_s, last + 1)
+    
+    unless pos.nil?
+      prior = pattern[last..(pos - 1)]
+      parts << prior unless parts.nil? or last.eql?(pos - 1)
+
+      parts << part.to_s unless last.eql?(pos - 1)
+    end
+  end
+  
+  parts << pattern[last..(pattern.size)]
+  
+  parts
+end
+
+# -------------------------------------------------------------
+def pattern_to_array(pattern, match_on, sub_out)
+  parts, part, pos, cntr = {}, "", 0, 1
+  
+  while !pos.nil? and !part.nil?
+    old_pos = pos + part.to_s.size unless part.nil?
+    
+    # Break up the incoming pattern by the {#} sections
+    part = pattern.match(match_on, old_pos + 1)
+    pos = pattern.index(part.to_s, old_pos + 1)
+
+    unless pos.nil?
+      before = pattern[old_pos..(pos - 1)]
+      parts[cntr] = [] if parts[cntr].nil?
+    
+      entry = part.to_s
+      sub_out.split("").each do |slice|
+        entry = entry.sub(slice, '')
+      end
+      
+      unless entry.nil?
+        existing = parts[cntr].find{|p, c| p.eql?(before)}
+      
+        if existing.nil?
+          parts[cntr] << {:pattern => before, :content => entry}
+        else
+          existing[:content] << entry
+        end
+      end
+      
+      cntr += 1
+    end
+  end
+
+  parts
+end
